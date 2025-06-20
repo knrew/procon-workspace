670c875f6251e7497eeaf1a36719b0c7233c5d8a
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-06-20 19:12:44 +0900

    update

--- bundle.sh
+++ bundle.sh
@@ -6,6 +6,9 @@ LIBRARY_DIR=~/codes/reprol/
 SOURCE_FILE=./src/main.rs
 SUBMISSION_FILE=./submission/submission.rs
 
+COMMENT="This module is bundled from:
+https://github.com/knrew/reprol"
+
 rustfmt $SOURCE_FILE
-library-bundler -l $LIBRARY_DIR $SOURCE_FILE > $SUBMISSION_FILE
-rustfmt $SUBMISSION_FILE
+library-bundler -l $LIBRARY_DIR -c "$COMMENT" $SOURCE_FILE > $SUBMISSION_FILE
+# rustfmt $SUBMISSION_FILE

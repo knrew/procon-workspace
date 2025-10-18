a972461e42e111e5d62b3639fe85c3e4e72cbff4
knrew <rew.7cc.12.1f@gmail.com>
2025-10-19 01:42:07 +0900

    rustc1.89.0に更新

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 #! /bin/env sh
 
-CARGO="cargo +1.70.0"
+CARGO="cargo +1.89.0"
 
 #
 # main.rs
@@ -14,10 +14,8 @@ build_main_release() {
   $CARGO build --release --bin main
 }
 
-# shellcheck disable=SC2034
 RUN_MAIN_DEBUG=./target/debug/main
 
-# shellcheck disable=SC2034
 RUN_MAIN_RELEASE=./target/release/main
 
 #
@@ -32,8 +30,6 @@ build_submission_release() {
   $CARGO build --release --package submission --bin submission
 }
 
-# shellcheck disable=SC2034
 RUN_SUBMISSION_DEBUG=./target/debug/submission
 
-# shellcheck disable=SC2034
 RUN_SUBMISSION_RELEASE=./target/release/submission
